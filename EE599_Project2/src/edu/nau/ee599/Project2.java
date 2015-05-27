1ee8fb1590c09caf46aacffea918cbf0f0c2a36d
jdk85 <jdk85@nau.edu>
2015-05-26 21:21:20 -0700

    Added methods to calculate difference in indoor vs outdoor as well as the change in indoor temperature over delta

--- EE599_Project2/src/edu/nau/ee599/Project2.java
+++ EE599_Project2/src/edu/nau/ee599/Project2.java
@@ -34,6 +34,9 @@ public class Project2 {
 			if(arg.equalsIgnoreCase("filter")){
 				(new Project2()).run();
 			}
+			if(arg.equalsIgnoreCase("difference")){
+				(new Project2()).calculateDifference();
+			}
 			else if(arg.equalsIgnoreCase("spoof")){
 				(new Project2()).plotFake();
 			}
@@ -56,6 +59,7 @@ public class Project2 {
 		System.out.println("See the included README for more info on each mode");
 		System.out.println("==== Modes ====");
 		System.out.println("filter - this mode parses the data and outputs a csv with tau estimates for 4/10/2015- 4/17/2015 filtered from 9PM to 5AM");
+		System.out.println("difference - mode calculates the difference between outdoor and indoor and the change in indoor temperate and generates a CSV");
 		System.out.println("spoof - this mode generates and plots a spoof model");
 		System.out.println("predict - this mode uses known MLE tau values for each wisard to predict the indoor temp");
 		System.out.println("arb - this mode plots the arb data");
@@ -274,6 +278,7 @@ public class Project2 {
 			////////////////////////////////////
 			estimateRC(shortList);
 			
+
 			
 			/////////////////////////////////////
 			//
@@ -295,10 +300,154 @@ public class Project2 {
 		catch(ParseException e){
 			e.printStackTrace();
 			System.exit(0);
-		} catch (IOException e) {
+		}catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
+	}
+	
+	/**
+	 * 
+	 */
+	public void calculateDifference(){
+		
+		//Create the node Array
+		nodes = new ArrayList<Node>();
+		//Add the arb data as the first entry with -1 id
+		nodes.add(new Node("arboretum",-1));
+		//Populate node list for wisards
+		for(int i = 1; i <= 19; i++){
+			nodes.add(new Node("wisard_" + i,i));
+		}
+		
+		//Load Adel data into the node array
+		parseAdel();
+		//Load Arb data into the node array
+		parseArb();		
+
+		/////////////////////////////////////
+		//
+		//		Filter Data by Time
+		//
+		////////////////////////////////////
+		
+		String startTime = "04/10/2015 21:00:00";
+		String endTime = "04/17/2015 5:00:00";
+		int start_hour = 21;
+		int end_hour = 5;
+		try{				
+			System.out.println("\r\n\r\n");
+			//Filter data by start and end date
+			//as well as start and end hour
+			ArrayList<Node> shortList = DataProcessor.getNodeSetDataRange(
+					sdf.parse(startTime).getTime(), 
+					sdf.parse(endTime).getTime(), 
+					start_hour, 
+					end_hour, 
+					nodes,
+					30); //Limit to only channels with more than 30 data points for the CLT
+			
+			/////////////////////////////////////
+			//
+			//		Generate a Data Set
+			//		MICHAEL - this method will output the CSV that you want 
+			//		TODO
+			////////////////////////////////////			
+			generateDataSet(shortList);
+			
+			
+			
+			System.out.println("Done");
+			
+		}
+		catch(ParseException e){
+			e.printStackTrace();
+			System.exit(0);
+		}
+	
+	}
+	public void generateDataSet(ArrayList<Node> nodes){
+
+		//Temp store the arboretum node
+		Node arboretum;
+		//Check that the node list has an arboretum node
+		if((arboretum = findNodeByName("arboretum")) == null){
+			System.out.println("Arboretum Node Not Found... Exiting");
+			System.exit(0);
+		}
+		double t0,t1,delta,difference,change;
+		Tuple arbSample;
+		long ts,ts1;
+		try{
+			FileWriter temperature = new FileWriter("TemperatureData.csv");
+			temperature.append("name,arb timestamp,arb temperature,adel timestamp, adel temperature,difference,change \n");
+			//Iterate through each node
+			for(Node n : nodes){
+				
+				//Make sure we're not using the arboretum node
+				if(!n.getName().equals("arboretum")){
+					ArrayList<TripleTuple> data = n.getEstimates();
+					
+					
+					/////////////////////////////////////
+					//
+					//		Calculate RC (Tau)
+					//
+					// Use this equation:
+					// tau = (-delta*(t0-e0))/(t1-t0)
+					//
+					// Where
+					// delta: time step (ts1-ts)/1000.0 in seconds
+					// t0 = indoor temp at time t
+					// t1 = indoor temp at time t+delta
+					// e0 = outdoor temp sample ts
+					// ts = time t
+					// ts1 = time at t+delta
+					//
+					//
+					////////////////////////////////////
+					for(int i = 1; i < data.size(); i++){
+						//Get the previous timestamp
+						ts = data.get(i-1).getTimestamp();
+						//Get the current timestamp
+						ts1 = data.get(i).getTimestamp();
+						
+						//Get the arb data temperature for the closest timestamp to the current (Te(t))
+						arbSample = getOutdoorTempAndTimestampAtTimeT(arboretum, ts);
+						//Fetch the temperature for previous room temp (Th(t))
+						t0 = data.get(i-1).getTemperature();
+						//Fetch the temperature for the current room temp (Th(delta+t))
+						t1 = data.get(i).getTemperature();
+						
+						//Filter out any outlier temperature values
+						if((t0 > 0 && t0 < 60) || (t1 > 0 && t1 < 60)){
+							delta = (ts1 - ts)/1000.;
+							//make sure delta is positive and not the same timestamp
+							if(delta > 0 && !(delta > 65)){
+								if(t1-t0 != 0){	
+									difference = Math.abs(t1-arbSample.getTemperature());
+									change = Math.abs(t1-t0)/delta;
+									temperature.append(n.getName() + "," + 
+											arbSample.getTimestamp() + "," + 
+											arbSample.getTimestamp() + "," + 
+											ts + "," +
+											t1 + "," +
+											difference + "," +
+											change);
+									
+								}
+							}
+						}
+					}
+				}
+			}
+			temperature.flush();
+			temperature.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	
 	}
 	/**
 	 * Tries to use the spoof data to estimate RC 
@@ -410,6 +559,35 @@ public class Project2 {
 		//System.out.println("Search Date: " + new Date(timestamp) + "\tFound Date: " + new Date(closestTuple.getTimestamp()));
 		
 		return closestTuple.getTemperature();
+	}
+	
+	/**
+	 * Matches the closest temporal temperature data point
+	 * @param outdoor - the Node object containing the arboretum data
+	 * @param timestamp - the timestamp to search for
+	 * @return temperature value of the closest timestamp
+	 */
+	public Tuple getOutdoorTempAndTimestampAtTimeT(Node outdoor, long timestamp){
+		//We need to search through the outdoor data array and get the closest timestamp
+		//This is highly inefficient (O(N)) since the data is already sorted so it's a 
+		//temporary solution
+		
+		//Init the closest Tuple object
+		TripleTuple closestTuple = null;
+		long difference = Long.MAX_VALUE;
+		long tempDifference;
+		for(TripleTuple t : outdoor.getEstimates()){
+			tempDifference = Math.abs(t.getTimestamp() - timestamp);
+			if( tempDifference < difference){
+				difference = tempDifference;
+				closestTuple = t;
+			}			
+		}
+		
+		//Debug testing
+		//System.out.println("Search Date: " + new Date(timestamp) + "\tFound Date: " + new Date(closestTuple.getTimestamp()));
+		
+		return new Tuple(closestTuple.getTimestamp(),closestTuple.getTemperature());
 	}
 	/**
 	 * Parse and filter the Adel CSV
